92af9e3f09dec548b89184e7c10789f82effa018
Anatoy Nosov <jomeisama@gmail.com>
2018-06-30 22:46:03 +0300

    Cc

--- 05/connecting_points.cpp
+++ 05/connecting_points.cpp
@@ -8,11 +8,18 @@
 
 using std::vector;
 using std::pow;
+using std::priority_queue;
 
 struct Edge {
   int from;
   int to;
   double length;
+};
+
+struct Point {
+  int x,y, index;
+  bool added = false;
+  void inspect() {std::cout << "(" << x <<":"<<y<<")";}
 };
 
 double length_between(int x1, int y1, int x2, int y2) {
@@ -65,30 +72,64 @@ vector<double> step(vector<vector<Edge>>& es, int current, vector<double>& weigh
   return weights;
 }
 
-double minimum_distance(vector<int> x, vector<int> y) {
-  double result = 0.;
-  // std::cout<< x.size() << std::endl;
-  vector<bool> added(x.size());
-  vector<vector<Edge>> es = edges(x, y);
-  vector<double> w(x.size());
-  for(int i = 0; i< w.size(); ++i) {w[i] = std::numeric_limits<int>::max();}
-  w[0] = 0;
-  w = step(es, 0, w);
-  
-  for(auto b : w) {
-    std::cout << b << " ";
-    result += b;
+vector<double> step_point(vector<Point>& points, int current, vector<double>& weights) {
+  points[current].added = true;
+  // std::cout<<"step " << current << points[current].added << "\n";
+  auto cmp = [&points, &current](Point left, Point right) { 
+    return length_between(points[current].x, points[current].y, left.x, left.y) >
+      length_between(points[current].x, points[current].y, right.x, right.y);
+  };
+  priority_queue<Point, vector<Point>, decltype(cmp)> h(cmp);
+  for(int i = 0; i < points.size(); ++i) {
+    if(points[i].added) {continue;}
+    h.push(points[i]);
   }
-  std::cout << std::endl;
+      // std::cout<<"cur:" << current << "\n";
+      double l; 
+  while(!h.empty()) {
+    Point p = h.top();
+    h.pop();
+    if(p.added) {continue;}
+    l = length_between(points[current].x, points[current].y, p.x, p.y);
+    if(weights[p.index] > l) {
+      // p.inspect();
+    // std::cout<<std::endl;
+    // std::cout << "set for "<< p.index <<" " << length_between(points[current].x, points[current].y, p.x, p.y) << "\n";
+    
+      weights[p.index] = l ;
+      step_point(points, p.index, weights);
+    }
+    
+  }
+  return weights;
+}
+
+double minimum_distance(vector<Point>& points) {
+  double result = 0.;
+  vector<double> w(points.size());
+  for(int i = 0; i < w.size(); ++i){
+    w[i] =  std::numeric_limits<int>::max();
+  }
+  w[0] = 0;
+
+  w = step_point(points, 0, w);
+  for(auto a: w) {
+    // std::cout << a << ' ';
+    result += a;
+  }
+  // std::cout << std::endl;
   return result;
 } 
 
 int main() {
   size_t n;
   std::cin >> n;
-  vector<int> x(n), y(n);
+  vector<Point> points;
   for (size_t i = 0; i < n; i++) {
-    std::cin >> x[i] >> y[i];
+    Point p = Point();
+    p.index = i;
+    std::cin >> p.x >> p.y;
+    points.push_back(p);
   }
-  std::cout << std::setprecision(10) << minimum_distance(x, y) << std::endl;
+  std::cout << std::setprecision(10) << minimum_distance(points) << std::endl;
 }
